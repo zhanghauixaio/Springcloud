828825229cf34e42e9ed0c80a0b748e8fed11eea
zhanghauixaio <zhanghauixaio@163.com>
2023-08-27 23:21:18 +0800

    config配置中心

---
+++ eureka-client/src/main/java/org/example/controller/ConfigController.java
@@ -0,0 +1,19 @@
+package org.example.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class ConfigController {
+
+    @Value("${username}")
+    String username;
+
+    @GetMapping("/getUser")
+    public String getUsername() {
+        return this.username;
+    }
+}
