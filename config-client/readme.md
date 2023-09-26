55cddeeb37ef6a97f480db7592a4325add0c289b
zhanghuaixiao <zhanghauixaio@163.com>
2023-09-26 14:53:04 +0800

    增加config-client动态刷新功能

---
+++ config-client/readme.md
@@ -0,0 +1,4 @@
+###动态刷新
++ 在pom中添加spring-boot-starter-actuator依赖
++ 在类上@RefreshScope
++ 用post方法执行/refresh端点
--- config-client/src/main/java/org/example/controller/FirstController.java
+++ config-client/src/main/java/org/example/controller/FirstController.java
@@ -1,9 +1,11 @@
 package org.example.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class FirstController {
     @Value("${username}")
