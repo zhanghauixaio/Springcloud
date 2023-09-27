0387373e8b40d25fbab3e4ee80ef794895e1d7a3
zhanghuaixiao <zhanghauixaio@163.com>
2023-09-27 17:36:22 +0800

    修改zuul模块，从config中动态加载，

--- zuul/src/main/java/org/example/ZuulApplication.java
+++ zuul/src/main/java/org/example/ZuulApplication.java
@@ -2,12 +2,37 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 @EnableZuulProxy
 @SpringBootApplication
 public class ZuulApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZuulApplication.class, args);
+    }
+
+    /**
+     * 自定义路由规则
+     *
+     * @return
+     */
+    @Bean
+    public PatternServiceRouteMapper serviceRouteMapper() {
+        // 建立类似/v1/userservice/**的路由规则
+        return new PatternServiceRouteMapper("(?<name>^.+)-(?<version>v.+$)", "${version}/${name}");
+    }
+
+    @Bean
+    @Primary
+    @RefreshScope
+    @ConfigurationProperties(prefix = "zuul")
+    public ZuulProperties zuulProperties() {
+        return new ZuulProperties();
     }
 }
